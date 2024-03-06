3628badad7806fac6a4cbf36ab9855c8bcce4239
abiral shrestha <abiral.shrestha36@gmail.com>
2024-03-07 00:20:12 +0545

    add

--- script.js
+++ script.js
@@ -84,3 +84,83 @@ function openTab(event, tabName) {
   document.getElementById(tabName).style.display = 'block';
   event.currentTarget.className += ' active';
 }
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+   
+  }
+  
+  else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  // document.getElementById("gps").innerHTML=
+  // `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24147.44170610748!2d${longitude}!3d${latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sne!2snp!4v1709747558674!5m2!1sne!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+
+  document.getElementById("gps").innerText="https://www.google.com/maps/@"+latitude+","+longitude;
+
+}
+
+
+function whatsapp(){
+            
+  // var address = document.getElementById("address").value;
+
+  // var name = document.getElementById("name").value;
+  // var number = document.getElementById("phonenumber").value;
+  // var gps = document.getElementById("address").value;
+  // // if(number==""){
+  // //     alert("Enter your Phone number");
+  // // }
+  // // else if(name==""){
+  // //     alert("Enter your Name");
+  // // }
+  // // else if(address==""){
+  // //     alert("Please Turn on  Google Map Service");
+  // // }
+  // // else
+  // // {
+
+  //   var name1 = document.getElementById( "name" ).value;
+  //   var phonenumber1= document.getElementById("phonenumber").value;
+  //   var address1= document.getElementById("address").value;
+
+      
+  //     //whatsapp ko lai
+  //      var url = "https://wa.me/9779816232444?text="+
+  // "----Billing information ----"+"%0a"+
+  // "Name :"+name1+"%0a"+
+  // "Address :"+address1+"%0a"+
+  // "Phone :"+phonenumber1+"%0a"+
+  // "Google Map:"+"gps"+"%0a"+
+  // "----Take My ORDER ----"+"%0a"+
+  // "Selected Dish : "+"%0a"+"cart-items"+"%0a"+
+  //  "Quantity : "+"quantity.innerText"+
+  //   "%0a"+
+  //   "Total : "+" total.innerText" +"%0a";
+  // window.open(url);
+
+  var address1 = document.getElementById("address").value;
+  var name1 = document.getElementById("name").value;
+  var phonenumber1 = document.getElementById("phonenumber").value;
+  var cartitem1= document.getElementById("cart-items").innerText;
+  var totals1= document.getElementById("total").innerText;
+  var gps1 =  document.getElementById("gps").value;
+  console.log(gps1);
+
+
+  var url = "https://wa.me/9779816232444?text="+"----Billing information ----"+"%0a"+"Name :"+name1+"%0a"+"Address :"+address1+"%0a"+
+  "Phone :"+phonenumber1+"%0a"+"----Take My ORDER ----"+"%0a"+"Selected Dish : "+"%0a"+cartitem1+"%0a"+totals1+"%0a"+"Delivery location :"+gps1;
+ if(gps1==""||name1==""||phonenumber1==""){
+  alert('Please complete form');
+}else{
+  window.open(url);
+ }
+  
+}
